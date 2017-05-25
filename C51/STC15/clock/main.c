09cc242f87ad8b999a1cb32235d24557ba6152ef
MaWei <1123265518@qq.com>
2017-05-25 23:16:38 +0800

    clock infrared

--- C51/STC15/clock/main.c
+++ C51/STC15/clock/main.c
@@ -42,6 +42,13 @@ u8 flashsec = 15;
 u8 flashIndex = 1;
 u8 dp = 0;
 
+//红外定义
+sbit IR_IO = P3^2;          // IR管脚 任意IO
+bit Irprot_LastState = 0;   // 端口状态位
+u8 codeCnt = 0;          // 数据码位计数
+u8 irTime;                   // 码时间，用于以125us时间计时
+u8 IR_data[4]={48,49,50,51}; // 接收数据缓存
+
 //定义闹钟
 typedef struct{
     unsigned char sMinute; //定时开始分钟
@@ -54,6 +61,7 @@ typedef struct{
  
 //声明函数
 void Timer0Init(void);//定时器0寄存器初始化
+void Timer2Init();//定时器2初始化
 void showNum(u8 dat,u8 num);//显示数据
 void UartInit(void);//串口初始化
 void SendData(u8 data_buf);//发送字符
@@ -73,8 +81,8 @@ void main(){
      
     //初始化定时器0
     Timer0Init();
+    Timer2Init();
     //初始化串口
-    UartInit();
     delayXms(1000);        
     Set_RTC();
     delayXms(1000);
@@ -175,54 +183,65 @@ void showNum(u8 dat,u8 num){
     }
 }
 
-////定时器0中断函数 125us
-//void Timer0() interrupt 1
-//{
-//   irTime++; //计时增加125us         	
-//   if(irTime==240) {irTime--;codeCnt=0x3f;} // ir解码后码值存放时间， 240*125us = 30ms  0x3f=64
-//   if(IR_IO)   Irprot_LastState=1; // 记录IO状态
-//   else if(Irprot_LastState)       // 有下降沿，并且上个状态是高电平，表示红外管收到数据
-//   {
-//      Irprot_LastState = 0;        // 下降沿后IO状态记录为0
-//      if(irTime<24) // 小于24*125us=3ms的间隔才进行处理	因为红外线的0的周期1.125ms，1的周期2.25ms
-//      {
-//         codeCnt++; //数据码位计数+1
-//		 codeCnt &= 0x1f; //等效if(codeCnt>0x1f) codeCnt=0x00; 这种操作比if判断更节约时间 0x1f=31
-//         IR_data[codeCnt>>3] <<= 1; //codeCnt>>3的范围(0~3)，等效于IR_data[i]向左移动1位 (i范围0~3)
-//         if( irTime>15 )  IR_data[codeCnt>>3]++;  //大于15*125us=1.875ms的间隔为数据1 ，1就+1，0就不变。
-//      }
-//      irTime = 0;                  // 下降沿处理完成，将时间清0
-//	  if(codeCnt==31) TR0=0,EX0=1;//解码完成后，掉定时器0，顺便打开中断0
-//   }	
-//}
-
-//void INT0_ISR(void) interrupt 0
-//{
-//	irTime=239;//给irTime赋予初始值，定时器0无法持续中断了，所以这里赋予默认值
-//	EX0=0; // INT0 暂时关闭
-//	TR0=1; //定时器0只有检测到有红外数据，才开始进行解码。
-//}
-
-//void INT0Init(void)//多了一个中断0
-//{
-//	IT0 = 0;	//INT0中断类型：1仅下降沿中断 0上下都中断
-//	EX0 = 1;	// 使能INT0中断
-//}
-
-//定时器0寄存器初始化
-void Timer0Init(void)        //1毫秒@12.000MHz
+//定时器0中断函数 125us
+void Timer0() interrupt 1
 {
-    AUXR |= 0x80;        //定时器时钟1T模式
-    TMOD &= 0xF0;        //设置定时器模式
-    TL0 = 0x20;        //设置定时初值
-    TH0 = 0xD1;        //设置定时初值
-    TF0 = 0;        //清除TF0标志
-    TR0 = 1;        //定时器0开始计时
-    ET0 = 1;
+   irTime++; //计时增加125us         	
+   if(irTime==240) {irTime--;codeCnt=0x3f;} // ir解码后码值存放时间， 240*125us = 30ms  0x3f=64
+   if(IR_IO)   Irprot_LastState=1; // 记录IO状态
+   else if(Irprot_LastState)       // 有下降沿，并且上个状态是高电平，表示红外管收到数据
+   {
+      Irprot_LastState = 0;        // 下降沿后IO状态记录为0
+      if(irTime<24) // 小于24*125us=3ms的间隔才进行处理	因为红外线的0的周期1.125ms，1的周期2.25ms
+      {
+         codeCnt++; //数据码位计数+1
+		 codeCnt &= 0x1f; //等效if(codeCnt>0x1f) codeCnt=0x00; 这种操作比if判断更节约时间 0x1f=31
+         IR_data[codeCnt>>3] <<= 1; //codeCnt>>3的范围(0~3)，等效于IR_data[i]向左移动1位 (i范围0~3)
+         if( irTime>15 )  IR_data[codeCnt>>3]++;  //大于15*125us=1.875ms的间隔为数据1 ，1就+1，0就不变。
+      }
+      irTime = 0;                  // 下降沿处理完成，将时间清0
+	  if(codeCnt==31) TR0=0,EX0=1;//解码完成后，掉定时器0，顺便打开中断0
+   }	
 }
- 
-//定时器0中断函数
-void T0() interrupt 1{
+
+//定时器0初始化
+void Timer0Init(void)		//125微秒@12.000MHz
+{
+	AUXR |= 0x80;		//定时器时钟1T模式
+	TMOD &= 0xF0;		//设置定时器模式
+	TL0 = 0x24;		//设置定时初值
+	TH0 = 0xFA;		//设置定时初值
+	TF0 = 0;		//清除TF0标志
+	TR0 = 1;		//定时器0开始计时
+    ET0=1;			//打开定时器0中断
+}
+
+//定时器2初始化
+void Timer2Init()		//1毫秒@12.000MHz
+{
+	AUXR |= 0x04;		//定时器时钟1T模式
+	T2L = 0x20;		//设置定时初值
+	T2H = 0xD1;		//设置定时初值
+	AUXR |= 0x10;		//定时器2开始计时
+    IE2 |= 0x04; //开始定时器2中断
+}
+//外部中断0初始化
+void INT0Init(void)//多了一个中断0
+{
+	IT0 = 0;	//INT0中断类型：1仅下降沿中断 0上下都中断
+	EX0 = 1;	// 使能INT0中断
+}
+
+//定时器0外部中断
+void INT0_ISR() interrupt 0
+{
+	irTime=239;//给irTime赋予初始值，定时器0无法持续中断了，所以这里赋予默认值
+	EX0=0; // INT0 暂时关闭
+	TR0=1; //定时器0只有检测到有红外数据，才开始进行解码。
+}
+
+//定时器2中断
+void T2() interrupt 12{
     second ++;
     changcode++;
     readInterval++;
@@ -242,17 +261,6 @@ void T0() interrupt 1{
         LedNum &= 0x03;//位操作，相当于0-3;
         flashsec++;
     }
-}
-
-//uart初始化
-void UartInit(void)		//4800bps@12.000MHz
-{
-	SCON = 0x50;		//8位数据,可变波特率
-	AUXR |= 0x01;		//串口1选择定时器2为波特率发生器
-	AUXR |= 0x04;		//定时器2时钟为Fosc,即1T
-	T2L = 0x8F;		//设定定时初值
-	T2H = 0xFD;		//设定定时初值
-	AUXR |= 0x10;		//启动定时器2
 }
 
 //发送一个字符
